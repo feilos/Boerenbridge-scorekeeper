efdd884ff5c095bf3e53a03672cf21a10da51d8f
feilos <wouterfeil@gmail.com>
2025-12-21 20:15:58 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BoerenBridgeScore from './BoerenBridgeScore'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BoerenBridgeScore />
+  </React.StrictMode>,
+)
